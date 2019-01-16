ff7f42155389162d10fb68fd15f932d5f4cc08f7
Max Tang <max.tang@accordo.com>
2019-01-16 15:45:51 +1300

    Added Chai

---
+++ src/components/__tests__/Test.test.js
@@ -0,0 +1,14 @@
+// const R = require("ramda");
+// const sinon = require("sinon");
+
+describe('Something', () => {
+  describe('not exists', () => {
+    it('should pass as if nothing has happened', () => {
+      expect(0).to.equal(0);
+    });
+  });
+
+  describe('with a fake server', () => {
+    it('should act as the real one', () => {});
+  });
+});
